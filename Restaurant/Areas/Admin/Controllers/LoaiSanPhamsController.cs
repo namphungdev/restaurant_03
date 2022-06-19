fae6a761d49fc933d19b3258217c0e9da62d8ce7
thanhdang2812 <narutothanh28@gmail.com>
2022-06-20 01:56:05 +0700

    Thanh 20/6

---
+++ Restaurant/Areas/Admin/Controllers/LoaiSanPhamsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Models;
+
+namespace Restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LoaiSanPhamsController : Controller
+    {
+        private readonly RestaurantContext _context;
+
+        public LoaiSanPhamsController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/LoaiSanPhams
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LoaiSanPhams.ToListAsync());
+        }
+
+        // GET: Admin/LoaiSanPhams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loaiSanPham = await _context.LoaiSanPhams
+                .FirstOrDefaultAsync(m => m.MaLoaiSanPham == id);
+            if (loaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(loaiSanPham);
+        }
+
+        // GET: Admin/LoaiSanPhams/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/LoaiSanPhams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaLoaiSanPham,TenLoaiSanPham")] LoaiSanPham loaiSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(loaiSanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: Admin/LoaiSanPhams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loaiSanPham = await _context.LoaiSanPhams.FindAsync(id);
+            if (loaiSanPham == null)
+            {
+                return NotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // POST: Admin/LoaiSanPhams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaLoaiSanPham,TenLoaiSanPham")] LoaiSanPham loaiSanPham)
+        {
+            if (id != loaiSanPham.MaLoaiSanPham)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(loaiSanPham);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LoaiSanPhamExists(loaiSanPham.MaLoaiSanPham))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: Admin/LoaiSanPhams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loaiSanPham = await _context.LoaiSanPhams
+                .FirstOrDefaultAsync(m => m.MaLoaiSanPham == id);
+            if (loaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(loaiSanPham);
+        }
+
+        // POST: Admin/LoaiSanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var loaiSanPham = await _context.LoaiSanPhams.FindAsync(id);
+            _context.LoaiSanPhams.Remove(loaiSanPham);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoaiSanPhamExists(int id)
+        {
+            return _context.LoaiSanPhams.Any(e => e.MaLoaiSanPham == id);
+        }
+    }
+}
---
+++ Restaurant/Areas/Admin/Controllers/ThucDonsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Models;
+
+namespace Restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ThucDonsController : Controller
+    {
+        private readonly RestaurantContext _context;
+
+        public ThucDonsController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/ThucDons
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ThucDons.ToListAsync());
+        }
+
+        // GET: Admin/ThucDons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thucDon = await _context.ThucDons
+                .FirstOrDefaultAsync(m => m.MaThucDon == id);
+            if (thucDon == null)
+            {
+                return NotFound();
+            }
+
+            return View(thucDon);
+        }
+
+        // GET: Admin/ThucDons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ThucDons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaThucDon,TenThucDon")] ThucDon thucDon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(thucDon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thucDon);
+        }
+
+        // GET: Admin/ThucDons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thucDon = await _context.ThucDons.FindAsync(id);
+            if (thucDon == null)
+            {
+                return NotFound();
+            }
+            return View(thucDon);
+        }
+
+        // POST: Admin/ThucDons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaThucDon,TenThucDon")] ThucDon thucDon)
+        {
+            if (id != thucDon.MaThucDon)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(thucDon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ThucDonExists(thucDon.MaThucDon))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thucDon);
+        }
+
+        // GET: Admin/ThucDons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thucDon = await _context.ThucDons
+                .FirstOrDefaultAsync(m => m.MaThucDon == id);
+            if (thucDon == null)
+            {
+                return NotFound();
+            }
+
+            return View(thucDon);
+        }
+
+        // POST: Admin/ThucDons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var thucDon = await _context.ThucDons.FindAsync(id);
+            _context.ThucDons.Remove(thucDon);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ThucDonExists(int id)
+        {
+            return _context.ThucDons.Any(e => e.MaThucDon == id);
+        }
+    }
+}
